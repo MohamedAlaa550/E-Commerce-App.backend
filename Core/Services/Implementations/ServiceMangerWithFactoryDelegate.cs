adcc0834e9134f1eb2900a29ac632dd1a1a247c0
MohamedAlaa550 <mohamedalaa.2675@gmail.com>
2025-11-27 17:46:39 +0200

    Factory Delegate

---
+++ Core/Services/Implementations/ServiceMangerWithFactoryDelegate.cs
@@ -0,0 +1,26 @@
+﻿using Services.Abstraction.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class ServiceMangerWithFactoryDelegate(Func<IProductService > _productService
+        ,Func<IBasketService> _basketService,
+       Func<IAuthenticationService> _authenticationService,
+        Func<IOrderService> _orderService,
+        Func<IPaymentService> _paymentService) : IServiceManger
+    {
+        public IProductService ProductService => _productService.Invoke();
+
+        public IBasketService BasketService => _basketService.Invoke();
+
+        public IAuthenticationService AuthenticationService => _authenticationService.Invoke();
+
+        public IOrderService OrderService => _orderService.Invoke();
+
+        public IPaymentService PaymentService => _paymentService.Invoke();
+    }
+}
--- E-Commerce-App/Extensions/CoreServiceExtensions.cs
+++ E-Commerce-App/Extensions/CoreServiceExtensions.cs
@@ -9,9 +9,37 @@ namespace E_Commerce_App.Extensions
     {
         public static IServiceCollection AddCoreServices (this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddScoped<IServiceManger, ServiceManger>();
+            services.AddScoped<IServiceManger, ServiceMangerWithFactoryDelegate>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IBasketService, BasketService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IPaymentService, PaymentService>();
             services.AddAutoMapper(o => { }, typeof(AssemblyReference).Assembly);
             services.Configure<JwtOptions>(configuration.GetSection("JwtOptions"));
+
+
+            services.AddScoped<Func<IProductService>> (provider =>
+            ()=> provider.GetRequiredService<IProductService>());
+
+
+            services.AddScoped<Func<IBasketService>>(provider =>
+         () => provider.GetRequiredService<IBasketService>());
+
+
+            services.AddScoped<Func<IAuthenticationService>>(provider =>
+         () => provider.GetRequiredService<IAuthenticationService>());
+
+
+            services.AddScoped<Func<IOrderService>>(provider =>
+         () => provider.GetRequiredService<IOrderService>());
+
+
+            services.AddScoped<Func<IPaymentService>>(provider =>
+         () => provider.GetRequiredService<IPaymentService>());
+
+
+
             return services;
         }
     }
